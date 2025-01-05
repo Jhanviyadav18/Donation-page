95797a027ddc0f4fdf64238db1303eaa6b6f5b41
JHANVI <jhanviyadav586@gmail.com>
2025-01-05 13:13:29 +0530

    Add files via upload

---
+++ don.js
@@ -0,0 +1,5 @@
+// Add dynamic behavior if necessary (e.g., alerts, transitions).
+document.getElementById('donateButton').addEventListener('click', () => {
+    alert('Thank you for your generosity!');
+  });
+  
